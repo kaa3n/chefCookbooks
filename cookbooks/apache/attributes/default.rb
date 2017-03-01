9021a4a63d57fc86b22b6733e134dc224cd08241
kaa3n <knedaria@gmail.com>
2017-03-01 15:05:05 +0000

    initial commit

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,12 @@
+default["apache"]["sites"]["knedaria2"] = { "site_title" => "Kaab2 website coming soon", "port" => 80, "domain" => "knedaria2.mylabserver.com" }
+default["apache"]["sites"]["knedaria2b"] = { "site_title" => "Kaabbb3 website coming soon", "port" => 80, "domain" => "knedaria2b.mylabserver.com" }
+default["apache"]["sites"]["knedaria3"] = { "site_title" => "Kaab ubuntu website coming soon", "port" => 80, "domain" => "knedaria3.mylabserver.com" }
+
+case node["platform"]
+when "centos"
+  default["apache"]["package"] = "httpd"
+when "ubuntu"
+  default["apache"]["package"] = "apache2"
+end
+
+ 
